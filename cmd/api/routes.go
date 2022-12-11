3242ee1933d0f1e462a161e061325d61bd222e8c
Daniel Furman <djfurman@gmail.com>
2022-12-11 00:04:19 -0500

    Add stub router.

---
+++ cmd/api/routes.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// specify permission for who can connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowCredentials: true,
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-Amzn-Trace-Id", "X-CSRF-Token"},
+		AllowedMethods:   []string{"DELETE", "GET", "POST", "PUT", "OPTIONS"},
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		ExposedHeaders:   []string{"Link"},
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	mux.Post("/mail", app.SendMail)
+	return mux
+}
